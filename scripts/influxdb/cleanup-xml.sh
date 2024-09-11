d969b6af0fc85f3db51a064684ccd1b407423a75
Stefan Kuhn <stefan.kuhn@akros.ch>
2024-09-11 20:05:19 +0200

    scripts: wip

---
+++ scripts/influxdb/cleanup-xml.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+echo 'clean up: delete all xml containing error'
+grep -Flr --include='*.xml' 'ch.admin.seco.eshab.application.publication.exception.PublicationNotFoundException' "$SCRIPT_DIR"/../current_portal/publications \
+  | xargs rm -f
+
+echo 'clean up: delete empty xml files'
+find "$SCRIPT_DIR"/../current_portal/publications -name '*.xml' -size 0 | xargs rm -f
--- scripts/influxdb/upload_all_publications.sh
+++ scripts/influxdb/upload_all_publications.sh
@@ -4,13 +4,7 @@ set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# clean up: delete all xml containing error
-grep -Flr --include='*.xml' 'ch.admin.seco.eshab.application.publication.exception.PublicationNotFoundException' "$SCRIPT_DIR"/../current_portal/publications" \
-  | xargs rm -f
-
-find "$SCRIPT_DIR"/../current_portal/publications" -name '*.xml' -size 0 | xargs rm -f
-
 for file in "$SCRIPT_DIR"/../current_portal/publications/*.xml; do
-  printf "Uploading $file\n"
+  echo "Uploading $file"
   "$SCRIPT_DIR/upload_publication.sh" "$file"
 done
