5743dbdf66f3d82766ec519983c95343f1bda4ee
Stefan Kuhn <stefan.kuhn@akros.ch>
2024-09-11 18:59:01 +0200

    scripts: wip

---
+++ scripts/influxdb/upload_all_publications.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+for file in "$SCRIPT_DIR"/../current_portal/publications/*.xml; do
+  printf "Uploading $file\n"
+  "$SCRIPT_DIR/upload_publication.sh" "$file"
+done
