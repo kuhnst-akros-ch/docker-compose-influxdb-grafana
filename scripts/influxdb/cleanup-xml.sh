7e0a3a7976f1e768bfe3bffa1517e27e89e93c3b
Stefan Kuhn <stefan.kuhn@akros.ch>
2024-09-11 20:55:29 +0200

    scripts: wip

--- scripts/influxdb/cleanup-xml.sh
+++ scripts/influxdb/cleanup-xml.sh
@@ -9,7 +9,10 @@ grep -Flr --include='*.xml' 'ch.admin.seco.eshab.application.publication.excepti
   | xargs rm -f
 
 echo 'clean up: delete empty xml files'
-find "$SCRIPT_DIR"/../current_portal/publications -name '*.xml' -size 0 | xargs rm -f
+find "$SCRIPT_DIR"/../current_portal/publications -type f -name '*.xml' -size 0 | xargs rm -f
 
 echo 'clean up: delete xml files not starting with "<?xml"'
-find "$SCRIPT_DIR"/../current_portal/publications -name "*.xml" -exec grep -L "^<?xml" {} + | xargs rm -f
+find "$SCRIPT_DIR"/../current_portal/publications -type f -name "*.xml" -exec grep -L "^<?xml" {} + | xargs rm -f
+
+echo 'clean up: delete large (>30 KB) xml files'
+find "$SCRIPT_DIR"/../current_portal/publications -type f -name "*.xml" -size +30k | xargs rm -f
